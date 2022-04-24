7514b53792e11d617bef51c473f4cf6930db0180
Luis <luis.mogueadp@gmail.com>
2022-04-24 11:33:55 -0500

    Se agrega proyecto

---
+++ index.js
@@ -0,0 +1,10 @@
+function getLargestNumber(num1, num2){
+    if(num1 > num2){
+        return num1
+    }
+    else{
+        return num2
+    }
+}
+console.log(getLargestNumber(56, 89))
+console.log(getLargestNumber(3, 4))
